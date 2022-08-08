a47e983cc48e574c2628ce86475c698b77e6fe7f
itsfaraz <faraz.abdul@anywhere.co>
2022-08-09 00:23:23 +0530

    queens in box

---
+++ src/backtracking/queenbasic/Queen.java
@@ -0,0 +1,23 @@
+package backtracking.queenbasic;
+
+public class Queen {
+    public static void main(String[] args) {
+        queenPermutation(new boolean[4],2,0,"");
+    }
+
+    public static int counter = 0;
+    public static void queenPermutation(boolean []boxes,int noOfQueens,int queenPlacedSoFar,String ans){
+        if (queenPlacedSoFar == noOfQueens) {
+            counter++;
+            System.out.println(counter+").\t "+ans) ;
+        }
+
+        for (int index = 0; index < boxes.length;index++){
+           if (!boxes[index]){
+               boxes[index] = true;
+             queenPermutation(boxes,noOfQueens,queenPlacedSoFar+1,ans+"q"+queenPlacedSoFar+"b"+index);
+               boxes[index] = false;
+           }
+        }
+    }
+}
